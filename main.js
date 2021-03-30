98a54ad81f9c0a50e1bb44dc1a631e93523b1813
SlumpG <shlomo.gangul1@gmail.com>
2021-03-30 10:21:28 +0300

    4th update

--- main.js
+++ main.js
@@ -4,29 +4,29 @@ async function getUsers() {
     let HTML = '';
 
     for (let user of Users) {
-        HTML += `<div id="${user['index']}" class="card "> `
+        HTML += `<div id="${user['index']}" class="card shadow m-1 mb-5 bg-white rounded""> `
         for (let userKey in user) {
             if (typeof user[userKey] === "object") {
-                HTML += `<h4  class="card-title">${userKey} : `
+                HTML += `<li class="list-group-item text-capitalize"><h5>${userKey}: `
                 for (let nameKey in user[userKey]) {
                     HTML += `${user[userKey][nameKey]}`
                 }
-                HTML += `</h4>`
+                HTML += `</h5></li>`
             } else if (userKey === 'picture') {
-                HTML += ` <img class="card-img-top" src="${user[userKey]}" >`
+                HTML += ` <img class="card-img-top" src="${user[userKey]}" > <ul class="list-group list-group-flush">`
             }
             else if (user[userKey] != user['_id'] && user[userKey] != user['picture'] && user[userKey] != user['index']) {
-                HTML += `<h5 class="card-text"> ${userKey} :${user[userKey]}</h5> `
+                HTML += `<li class="list-group-item text-capitalize"> ${userKey}: ${user[userKey]}</li> `
             }
         }
-        HTML += `</div>`
+        HTML += ` </ul></div>`
 
     }
     mainDiv.innerHTML += HTML
 
     infoInTableForm.addEventListener('click', () => {
         HTML = ''
-        HTML += `<table class = "card" > `
+        HTML += `<table class="table table-hover table-dark" > `
 
         for (let user of Users) {
             if (user['index'] < 1) {
@@ -34,7 +34,7 @@ async function getUsers() {
 
                 for (let thKey in user) {
                     if (user[thKey] != user['_id'] && user[thKey] != user['index']) {
-                        HTML += ` <th> ${thKey}</th>`
+                        HTML += ` <th scope="col"> ${thKey}</th>`
 
                     }
                 }
@@ -67,22 +67,49 @@ async function getUsers() {
 
     register.addEventListener('click', () => {
         HTML = ''
-        HTML += `<form id="registerForm" class="card">
-        <label for="fname">First name:</label><br>
-        <input type="text" id="fname" name="fname" required><br>
-        <label for="lname">Last name:</label><br>
-        <input type="text" id="lname" name="lname" required><br>
-        <label for="email"> email:</label><br>
-        <input type="email" id="email" name="email" required><br>
-        <label for="email2"> re email:</label><br>
-        <input type="email" id="email2" name="email2" required><br>
-        <label for="phone"> phone number:</label><br>
-        <input type="text" id="phone" name="phone" required><br>
-        <label for="age">age:</label><br>
-        <input type="text" id="age" name="age" required><br><br>
-        <input class="btn btn-secondary" id="submitForm" type="button" value="Submit">
-        <p id="errorMsg"><p>
-      </form> `
+        HTML += `<form id="registerForm" class="mx-auto ">
+    <div class="form-row">
+        <div class="col-md-6 mb-3 ">
+            <label class="text-light" for="fname">First name:</label><br/>
+            <input type="text" id="fname" name="fname" required>
+        </div>
+
+        <div class="col-md-6 mb-3" >
+            <label class="text-light" for="lname"">Last name:</label><br/>
+            <input type="text" id="lname" name="lname" required>
+        </div>
+    </div>
+    <div class="form-row">
+        <div class="col-md-6 mb-3 ">
+            <label class="text-light" for="email"> email:</label><br/>
+            <input type="email" id="email" name="email" required>
+   
+        </div>
+        <div class="col-md-6 mb-3 ">
+            <label class="text-light" for="email2"> re email:</label><br/>
+            <input type="email" id="email2" name="email2" required>
+  
+        </div>
+    </div>
+    <div class="form-row">
+        <div class="col-md-6 mb-3 ">
+            <label class="text-light" for="phone"> phone number:</label><br/>
+            <input type="text" id="phone" name="phone" required>
+        </div>
+        <div class="col-md-6 mb-3 ">
+            <label class="text-light" for="age">age: </label><br/>
+            <input type="text" id="age" name="age" required>
+    
+        </div>
+    </div>
+    <div class="form-row">
+    <div class="col-md-6 mb-3  ">
+        <button class="btn btn-secondary" id="submitForm" type="button" >Submit</button>
+        <p class="bg-danger text-light" id="errorMsg"><p>
+        </div>
+        </div>
+      </form>
+       `
 
 
         mainDiv.innerHTML = HTML
@@ -133,8 +160,8 @@ getUsers();
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  function closeNav() {
+}
+
+function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-  }
+}
